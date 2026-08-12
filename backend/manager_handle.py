6692adb36c58d6dad4b3936ee51cc0adfa56ae1b
KunwarveerSingh7 <kunwarveer.Cheema@student.torrens.edu.au>
2026-08-12 13:11:33 +1000

    Implement manager user creation script
    Add script to create a manager user if it doesn't exist.

---
+++ backend/manager_handle.py
@@ -0,0 +1,27 @@
+from database import SessionLocal, Base, engine
+from models import User
+from auth_utils import hash_password
+
+Base.metadata.create_all(bind=engine)
+db = SessionLocal() 
+
+email = "manager@meetingminute.com"
+password = "Manager123!"
+existing_user = db.query(User).filter(User.email == email).first()
+
+if existing_user:
+    print(f"Manager account with email '{email}' already exists.")
+else:
+    manager_user = User(
+        email=email,    
+        hashed_password=hash_password(password),
+        role="manager"  
+    )
+    db.add(manager_user)    
+    db.commit()
+
+    print ("Manager account created successfully.")
+    print (f"Email: {email}")
+    print (f"Password: {password}")
+
+db.close()
